9ebef78024a1c10df2db37b0f0ad5410e5219a0c
SmallFryHero <116933508+SmallFryHero@users.noreply.github.com>
2023-01-22 00:56:05 -0800

    Update klass.data.js

--- app/core/klasses/klass.data.js
+++ app/core/klasses/klass.data.js
@@ -23,7 +23,7 @@ function classLevelUnlockFactory(klass, level) {
 exports.KLASSES = [
     {
         // perk: double apts until level 10
-        name: 'Doogler',
+        name: 'Peasant',
         aptitudes: index_1.uniformSkillMap(.5),
         img: 'ruffian.png',
         hint: "??? You shouldn't be reading this...",
