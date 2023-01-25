c171cc6161cb7d649375c42175cbf1b4746e83f2
SmallFryHero <116933508+SmallFryHero@users.noreply.github.com>
2023-01-24 18:40:04 -0800

    Delete rxjs-operators.ts

--- app/rxjs-operators.ts
+++
@@ -1,9 +0,0 @@
-
-import 'rxjs/add/observable/interval';
-import 'rxjs/add/observable/timer';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/take';
-
-// XXX: just used for testing
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/scan';
