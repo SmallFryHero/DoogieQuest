9aaecf43a53b3c2b8c6524bc99a31bf533c3cd32
SmallFryHero <116933508+SmallFryHero@users.noreply.github.com>
2023-01-24 18:31:09 -0800

    Delete index.ts

--- app/core/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './skills/index';
-export * from './klasses/index';
-export * from './stats/index';
-export * from './zones/index';
-export * from './game/index';
-export * from './perks/index';
-
-export * from './utils';
