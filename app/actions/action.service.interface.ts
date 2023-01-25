621901375a6d79c38755f452baab41f99fc4aac5
SmallFryHero <116933508+SmallFryHero@users.noreply.github.com>
2023-01-24 17:50:40 -0800

    Delete action.service.interface.ts

--- app/actions/action.service.interface.ts
+++
@@ -1,9 +0,0 @@
-import { LiveZoneAction, ProtoActionOutcome } from '../core/index';
-import { Observable } from 'rxjs/Observable';
-
-export interface IActionService {
-    currentAction: LiveZoneAction;
-    actionSpeedMultiplier: number;
-    protoActionOutcomeSubject: Observable<ProtoActionOutcome>;
-    inexpMultiplier: number;
-}
